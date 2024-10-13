0614ef96a51d895d30fddce402c59dd5ade94115
aaronbrezel <aaronbrezel@gmail.com>
2024-10-13 10:43:43 -0400

    cleanup

--- recommenado/tornadoserver.py
+++ recommenado/tornadoserver.py
@@ -1,38 +1,3 @@
-#!/usr/bin/env python3
-"""Local News Lab Tech Lead At-home Task start module.
-
-We use the Tornado web server for our article recommendations API.
-https://www.tornadoweb.org/en/stable/index.html
-
-Please start your Tornado web app with this file. But feel free to modify this
-file as much as you'd like and to create additional modules if you'd like.
-
-Currently, this setup assumes a simple API request like this:
-
-http://localhost:8888/?xyz=123&abc=456
-
-But please modify the API request format as much as you'd like.
-
-Note that we've included the function read_db() to retrieve the data from
-articlesembeds.csv, which is our article "database". There are 4 rows of data.
-There are two columns in this "database":
-Article ID
-Embedding (from Google's Gemini API)
-
-Please use the RequestHandler write() method to return the API response.
-This will write it to the browser and make it easy for us to see how you're
-structuring the response.
-
-Again, we're kindly requesting only 2 to 3 hours of your time. If you'd like, you
-can note areas where you'd expand or do things differently with more time and
-resources. This is a conversation starter to show us how you think and work!
-
-Please feel free to contact me (Eric) with any questions:
-eric.evan.chen@columbia.edu
-
-Have fun! =)
-"""
-
 """
 Implementation notes:
 * Two problems here: 
